36f80c0ebc3dbbbb240e8ae86b4b55b352babd14
MinhDuc <duclmhe173155@fpt.edu.vn>
2025-06-21 15:28:28 +0700

    djtmedoi

--- src/config/passport.js
+++ src/config/passport.js
@@ -3,10 +3,13 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const Users = require('../api/models/user');
 require('dotenv').config();
 
+const frontendUrl = process.env.FRONTEND_URL || 'http://localhost:5173'
+
+
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: 'http://localhost:3000/api/user/auth/google/callback',
+    callbackURL: `${frontendUrl}/api/user/auth/google/callback`,
     scope: ['profile', 'email']
   },
   async (accessToken, refreshToken, profile, done) => {
